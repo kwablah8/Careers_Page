b7c8da13346837a1e88f806d9401c6e0cfdd05ca
93n105 <ernestkwablah19@gmail.com>
2024-03-13 13:25:18 +0000

    create dictionary of jobs

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Accra, Ghana',
+        'salary': '4500'
+    },
+    {
+        'id': 1,
+        'title': 'Software Engineer',
+        'location': 'Accra, Ghana',
+        'salary': '5000'
+    }
+]
 
 
 @app.route('/')
 def home_page():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
 
 
 if __name__ == '__main__':
